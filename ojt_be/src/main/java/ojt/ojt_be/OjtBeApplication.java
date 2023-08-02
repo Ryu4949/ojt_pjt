e94f36b10961d68e505a251518bd3464e79f2c19
Ryu4949 <kyong494949@gmail.com>
2023-08-02 16:59:53 +0900

    [Add] initial commit for ojt_be

---
+++ ojt_be/src/main/java/ojt/ojt_be/OjtBeApplication.java
@@ -0,0 +1,13 @@
+package ojt.ojt_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjtBeApplication.class, args);
+	}
+
+}
