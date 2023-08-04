55dee076e210acd3658599ee9b46cc96f029c922
Ryu4949 <kyong494949@gmail.com>
2023-08-04 10:17:57 +0900

    [Add] add users service

---
+++ ojt_be/src/main/java/ojt/ojt_be/biz/users/service/UsersService.java
@@ -0,0 +1,10 @@
+package ojt.ojt_be.biz.users.service;
+
+import ojt.ojt_be.biz.users.repository.UsersRepositoryImpl;
+
+public class UsersService {
+
+    private final UsersRepositoryImpl usersRepository = new UsersRepositoryImpl();
+
+
+}
