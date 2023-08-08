0bfcc2cb06800d656d1c255c3ac15ceac0ed7764
Ryu4949 <kyong494949@gmail.com>
2023-08-08 13:40:47 +0900

    [Add] initial commit for maven project

---
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/MavenPjtApplication.java
@@ -0,0 +1,13 @@
+package maven.maven_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenPjtApplication.class, args);
+	}
+
+}
---
+++ maven_pjt/maven_pjt/src/test/java/maven/maven_pjt/MavenPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package maven.maven_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
