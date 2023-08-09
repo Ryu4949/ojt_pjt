907e1266d39871b0dd47789e7af1e45732dd09f7
Ryu4949 <kyong494949@gmail.com>
2023-08-09 09:42:43 +0900

    [Chore] add db setting & schema and data sql file

--- maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/MavenPjtApplication.java
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/MavenPjtApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MavenPjtApplication {
+public class 	MavenPjtApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenPjtApplication.class, args);
