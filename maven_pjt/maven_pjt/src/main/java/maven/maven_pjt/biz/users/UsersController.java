45e03d5fb6031df37082a03efe819a4cb151bdd5
Ryu4949 <kyong494949@gmail.com>
2023-08-10 10:09:41 +0900

    [Feat] get all users

--- maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/users/UsersController.java
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/users/UsersController.java
@@ -1,13 +1,12 @@
 package maven.maven_pjt.biz.users;
 
 import maven.maven_pjt.biz.users.dto.UsersInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import maven.maven_pjt.biz.users.UsersMapper.*;
 
 import java.util.List;
 
@@ -15,11 +14,14 @@ import java.util.List;
 @RequestMapping("/user-service")
 public class UsersController {
 
+    @Autowired
+    UsersService usersService;
+
     @GetMapping("/users")
     public ResponseEntity getAllUsers() {
         HttpStatus status = HttpStatus.OK;
-        List<UsersInfoDto> result = findAllUsers
-
-        return new ResponseEntity<>(status);
+        List<UsersInfoDto> result = usersService.findAllUsersInfo();
+//        String result = "Hello World";
+        return new ResponseEntity<>(result, status);
     }
 }
--- maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/users/UsersService.java
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/users/UsersService.java
@@ -1,13 +1,18 @@
 package maven.maven_pjt.biz.users;
 
 import maven.maven_pjt.biz.users.dto.UsersInfoDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UsersService {
+
+    @Autowired
+    UsersMapper usersMapper;
+
     public List<UsersInfoDto> findAllUsersInfo() {
-        return ;
+        return usersMapper.findAllUsers();
     }
 }
