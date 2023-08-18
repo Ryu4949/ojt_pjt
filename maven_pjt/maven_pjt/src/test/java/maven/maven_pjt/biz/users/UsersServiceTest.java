21c8b7358b0dcadab171aa0869914700c25f421b
Ryu4949 <kyong494949@gmail.com>
2023-08-18 17:11:27 +0900

    [Add] create test class and mark display name

---
+++ maven_pjt/maven_pjt/src/test/java/maven/maven_pjt/biz/users/UsersServiceTest.java
@@ -0,0 +1,44 @@
+package maven.maven_pjt.biz.users;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsersServiceTest {
+
+    @DisplayName("사용자Id로 사용자 찾기")
+    @Test
+    void getUserById() {
+    }
+
+    @DisplayName("모든 사용자 조회하기")
+    @Test
+    void findAllUsersInfo() {
+    }
+
+    @DisplayName("사용자 상세 정보 조회하기")
+    @Test
+    void getUserDetail() {
+    }
+
+    @DisplayName("사용자 정보 수정하기")
+    @Test
+    void updateUser() {
+    }
+
+    @DisplayName("사용자 삭제하기")
+    @Test
+    void deleteUser() {
+    }
+
+    @DisplayName("회원가입하기")
+    @Test
+    void signUpUser() {
+    }
+
+    @DisplayName("새로운 사용자 Id(PK)값 반환하기")
+    @Test
+    void getNewUserId() {
+    }
+}
