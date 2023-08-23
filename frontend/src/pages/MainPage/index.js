015554732c4ff669318aeaa35ddf969120005c1e
Ryu4949 <kyong494949@gmail.com>
2023-08-23 10:25:46 +0900

    [Add] set form to check api

--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const MainPage = () => {
+    const [searchId, setSearchId] = useState(0);
+    
+    useEffect(() => {
+    })
+    
+    const handleChange = (e) => {
+        setSearchId(e.target.value);
+    }
+
   return (
     <div>
-      <h1>Main Page입니다.</h1>
+        <h1>Main Page입니다.</h1>
+        <input 
+            onChange={handleChange}
+            className='nav__input' type="text" placeholder="검색해주세요."/>
+        <button>검색</button>
     </div>
   )
 }
