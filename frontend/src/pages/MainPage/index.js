560dbafe3a62f4ef35e78ee3c701a350f8ebc4db
Ryu4949 <kyong494949@gmail.com>
2023-08-23 10:44:15 +0900

    [Add] connect get-user-info api

--- frontend/src/pages/MainPage/index.js
+++ frontend/src/pages/MainPage/index.js
@@ -1,13 +1,27 @@
 import React, { useEffect, useState } from 'react'
+import axios from '../../api/axios';
+import requests from '../../api/requests';
 
 const MainPage = () => {
     const [searchId, setSearchId] = useState(0);
-    
+
     useEffect(() => {
+        if(searchId > 0) {
+            getUserDetail(searchId)
+        }
     })
     
     const handleChange = (e) => {
         setSearchId(e.target.value);
+    }
+
+    const getUserDetail = async (userId) => {
+        try {
+            const response = await axios.get(`/user-service/users/${userId}`);
+            console.log(response);
+        } catch(error) {
+            console.log(error);
+        }
     }
 
   return (
@@ -16,7 +30,7 @@ const MainPage = () => {
         <input 
             onChange={handleChange}
             className='nav__input' type="text" placeholder="검색해주세요."/>
-        <button>검색</button>
+        <button onClick={getUserDetail}>검색</button>
     </div>
   )
 }
