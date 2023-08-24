864eae9c10253e5281b6873c35953eb98817af1a
Ryu4949 <kyong494949@gmail.com>
2023-08-24 15:26:26 +0900

    [Delete] delete frontend to seperate repository

--- frontend/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-    baseURL: "http://localhost:8080",
-    content_type: "application/json"
-})
-
-export default instance;
--- frontend/src/api/requests.js
+++
@@ -1,5 +0,0 @@
-const requests = {
-    getUserDetail: "/user-service/users"
-}
-
-export default requests;
