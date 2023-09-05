fa9f1c602a7551fb9a1bc16479e6d176edb1d242
Ryu4949 <kyong494949@gmail.com>
2023-09-05 13:35:08 +0900

    [Add] add jwt utils

--- maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/jwt/JwtUtils.java
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/jwt/JwtUtils.java
@@ -6,32 +6,46 @@ import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.Jwts;
 import maven.maven_pjt.biz.user.entity.User;
 import org.springframework.data.util.Pair;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.security.Key;
 import java.util.Date;
 
 public class JwtUtils {
 
-    public static String getUsername(String token) {
-        return Jwts.parserBuilder()
-                .setSigningKeyResolver(SigningKeyResolver.instance)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
+    private JwtUtils() { }
+
+    public static int getCurrentMemberID() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getName() == null) {
+            throw new RuntimeException("SecurityContext에 인증 정보가 없습니다.");
+        }
+
+        return Integer.parseInt(authentication.getName());
     }
 
-    public static String createToken(User user) {
-        Claims claims = Jwts.claims().setSubject(user.getUserId()); // subject
-        Date now = new Date(); // 현재 시간
-        Pair<String, Key> key = JwtKey.getRandomKey();
-        // JWT Token 생성
-        return Jwts.builder()
-                .setClaims(claims) // 정보 저장
-                .setIssuedAt(now) // 토큰 발행 시간 정보
-                .setExpiration(new Date(now.getTime() + JwtTokenProvider.ACCESS_TOKEN_EXPIRE_TIME)) // 토큰 만료 시간 설정
-                .setHeaderParam(JwsHeader.KEY_ID, key.getFirst()) // kid
-                .signWith(key.getSecond()) // signature
-                .compact();
-    }
+//    public static String getUsername(String token) {
+//        return Jwts.parserBuilder()
+//                .setSigningKeyResolver(SigningKeyResolver.instance)
+//                .build()
+//                .parseClaimsJws(token)
+//                .getBody()
+//                .getSubject();
+//    }
+//
+//    public static String createToken(User user) {
+//        Claims claims = Jwts.claims().setSubject(user.getUserId()); // subject
+//        Date now = new Date(); // 현재 시간
+//        Pair<String, Key> key = JwtKey.getRandomKey();
+//        // JWT Token 생성
+//        return Jwts.builder()
+//                .setClaims(claims) // 정보 저장
+//                .setIssuedAt(now) // 토큰 발행 시간 정보
+//                .setExpiration(new Date(now.getTime() + JwtTokenProvider.ACCESS_TOKEN_EXPIRE_TIME)) // 토큰 만료 시간 설정
+//                .setHeaderParam(JwsHeader.KEY_ID, key.getFirst()) // kid
+//                .signWith(key.getSecond()) // signature
+//                .compact();
+//    }
 }
