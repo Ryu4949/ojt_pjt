a958438adb723e708c9ddd3e615aae39c42c6c9e
Ryu4949 <kyong494949@gmail.com>
2023-09-08 11:27:58 +0900

    [Add] note apis

--- maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/note/NoteController.java
+++ maven_pjt/maven_pjt/src/main/java/maven/maven_pjt/biz/note/NoteController.java
@@ -1,4 +1,33 @@
 package maven.maven_pjt.biz.note;
 
+import lombok.Data;
+import maven.maven_pjt.biz.note.entity.Note;
+import maven.maven_pjt.biz.user.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Data
+@RequestMapping("/notes")
 public class NoteController {
+
+    @GetMapping
+    public void getAllNotes() {}
+
+    @GetMapping
+    public void getNoteDetail() {}
+
+    @PostMapping
+    public void createNote() {}
+
+    @PutMapping
+    public void updateNote() {}
+
+    @DeleteMapping
+    public void deleteNote() {}
+
+
+
 }
